506720bf0d6b671675e259436f7adf5765bd632f
sivaprasad <sivaprasadtppba@gmail.com>
2022-11-14 16:43:07 +0530

    unique id set in previousId model

--- src/main/java/com/library/backend/model/PreviousId.java
+++ src/main/java/com/library/backend/model/PreviousId.java
@@ -1,5 +1,6 @@
 package com.library.backend.model;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
@@ -7,6 +8,8 @@ import lombok.Data;
 @Data
 @Document(collection = "previousIds")
 public class PreviousId {
+    @Id
+    private String id; // to make sure the same document gets updated with incremented id's
     private String type;
     private Integer previousId;
 }
